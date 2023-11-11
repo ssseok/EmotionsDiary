e6e587416cf244938b5ccb4382c24a7427340909
Hyun Seok Park <luckseok1@gmail.com>
2023-11-12 00:47:04 +0900

    style: overflow-auto

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -3,7 +3,7 @@ import { type LayoutProps } from "../../interface/type";
 export default function Layout({ children }: LayoutProps) {
   return (
     <div className="h-full overflow-hidden py-6 px-4 bg-[url(../public/images/bg.svg)] text-mood-gray-700">
-      {children}
+      <div className="h-full overflow-auto">{children}</div>
     </div>
   );
 }
